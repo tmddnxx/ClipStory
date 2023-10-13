6a303e9d6bcc57d7722744a3a82703405266f8d5
suhong <zaqxsw327@naver.com>
2023-10-13 18:24:35 +0900

    해야할것

--- src/main/java/com/example/movie/model/dao/AdminDAO.java
+++ src/main/java/com/example/movie/model/dao/AdminDAO.java
@@ -1,5 +1,29 @@
 package com.example.movie.model.dao;
 
 public class AdminDAO {
-    /*굿*/
+
+//    addCrew // 제작진 제작 - 은석
+//    addCast // 출연정보 제작 - 은석
+//    addPhoto 제작 - 은석
+//    addMovie 수정 - 은석
+
+
+//    (super)login 복붙 - 종원
+//    memberList 제작 - 종원
+
+
+//    boardList 복붙 - 승우
+//    addBoard 복봍 - 승우
+//    boardView 복붙 - 승우
+//    removeBoard 복붙 - 승우
+//    commentList 복붙 - 승우
+//    removeComment 복붙 - 승우
+
+
+//    movieList 수정 - 수홍
+//    removeMovie 복붙  - 수홍
+//    modifyMovie 복붙 -수홍
+//    movieView 복붙 - 수홍
+//    reviewList 복붙 - 수홍
+//    removeReview 복붙 - 수홍
 }
--- src/main/java/com/example/movie/service/AdminService.java
+++ src/main/java/com/example/movie/service/AdminService.java
@@ -1,28 +1,28 @@
 package com.example.movie.service;
 
 public class AdminService {
-//    login
-
-//    movieList
-//    addMovie
-//    removeMovie
-//    modifyMovie
-//    movieView
-//    addCrew // 제작진
-//    addCast // 출연정보
-//    addPhoto
+//    addCrew // 제작진 제작 - 은석
+//    addCast // 출연정보 제작 - 은석
+//    addPhoto 제작 - 은석
+//    addMovie 수정 - 은석
 
 
-//    boardList
-//    addBoard
-//    boardView
-//    removeBoard
+//    (super)login 복붙 - 종원
+//    memberList 제작 - 종원
 
-//    commentList
-//    removeComment
 
-//    reviewList
-//    removeReview
+//    boardList 복붙 - 승우
+//    addBoard 복봍 - 승우
+//    boardView 복붙 - 승우
+//    removeBoard 복붙 - 승우
+//    commentList 복붙 - 승우
+//    removeComment 복붙 - 승우
 
-//    memberList
+
+//    movieList 수정 - 수홍
+//    removeMovie 복붙  - 수홍
+//    modifyMovie 복붙 -수홍
+//    movieView 복붙 - 수홍
+//    reviewList 복붙 - 수홍
+//    removeReview 복붙 - 수홍
 }
